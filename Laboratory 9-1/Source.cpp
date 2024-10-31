fc6b46498da25d366448458a7aa2d8924f573fa3
Connor Apple <c-apple@onu.edu>
2024-10-31 10:21:44 -0400

    Completed printAllElements Function

--- Laboratory 9-1/Source.cpp
+++ Laboratory 9-1/Source.cpp
@@ -1,1 +1,19 @@
-/
+#include <iostream>
+#include <stdlib> 
+
+void printAllElements(int arr[], int size);
+void printEvenIndices(int arr[], int size);
+void printEvenValues(int arr[], int size);
+void printReversedOrder(int arr[], int size);
+void printFirstAndLast(int arr[], int size);
+
+int main() {
+
+}
+void printAllElements(int arr[], int size) {
+	for (i=0; i < size; i++){
+		cout << arr[i] << "";
+	}
+}
+
+}
