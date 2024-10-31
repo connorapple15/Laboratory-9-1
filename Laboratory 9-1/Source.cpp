7ceb2ee3b574177be38c10ff55438e9236802430
Connor Apple <c-apple@onu.edu>
2024-10-31 10:36:32 -0400

    Completed P9-1

--- Laboratory 9-1/Source.cpp
+++ Laboratory 9-1/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <stdlib> 
-
+#include <cstdlib> 
+#include <ctime>
+using namespace std;
 void printAllElements(int arr[], int size);
 void printEvenIndices(int arr[], int size);
 void printEvenValues(int arr[], int size);
@@ -8,30 +9,56 @@ void printReversedOrder(int arr[], int size);
 void printFirstAndLast(int arr[], int size);
 
 int main() {
+	const int SIZE = 10;
+	int arr[SIZE];
 
+	srand(static_cast<unsigned int>(time(0)));
+
+	for (int i = 0; i < SIZE; i++) {
+		arr[i] = rand() % 9000 + 1000;
+	}
+	cout << "The random integers: ";
+	printAllElements(arr, SIZE);
+
+	cout << "Even indices: ";
+	printEvenIndices(arr, SIZE);
+
+	cout << "Even values: ";
+	printEvenValues(arr, SIZE);
+
+	cout << "Reversed order: ";
+	printReversedOrder(arr, SIZE);
+
+	cout << "First and last: ";
+	printFirstAndLast(arr, SIZE);
 }
 void printAllElements(int arr[], int size) {
-	for (i=0; i < size; i++){
-		cout << arr[i] << "";
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
 void printEvenIndices(int arr[], int size) {
-	for (i = 0; i < size; i += 2) {
-		cout << arr[i] << "";
+	for (int i = 0; i < size; i += 2) {
+		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
 void printEvenValues(int arr[], int size) {
-	for (i = 0; i < size; i++) {
-		if (arr[i] % 2 = 0) {
-			cout << arr[i] << "";
+	for (int i = 0; i < size; i++) {
+		if (arr[i] % 2 == 0) {
+			cout << arr[i] << " ";
 		}
 	}
+	cout << endl;
 }
 void printReversedOrder(int arr[], int size) {
-	for (i = size - 1; i >= 0; i--) {
-		cout << arr[i] << "";
+	for (int i = size - 1; i >= 0; i--) {
+		cout << arr[i] << " ";
 	}
+	cout << endl;
 }
 void printFirstAndLast(int arr[], int size) {
-	cout << arr[0] << "" << arr[size - 1];
+	cout << arr[0] << " " << arr[size - 1];
+	cout << endl;
 }
